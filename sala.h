9154378aaba277aa6d6807a698d9987772d874eb
andrenfortes <andrefortes2006@gmail.com>
2018-07-09 18:07:32 -0300

    Create sala.h

---
+++ sala.h
@@ -0,0 +1,46 @@
+#pragma once
+class salaesq
+{
+public:
+	salaesq();
+	~salaesq();
+	examinar(char obj);
+	bool dir();
+private:
+	int aberta;
+};
+
+class saladir
+{
+public:
+	saladir();
+	~saladir();
+	examinar();
+	bool esq();
+private:
+	int aberta;
+};
+
+class salameio
+{
+public:
+	salameio();
+	~salameio();
+	bool dir();
+	bool esq();
+private:
+};
+/* objetos: 
+	- Lampião // Que suas noites sejam tão brilhantes quanto suas futuras ideias
+	- Abajur //Ainda com medo de escuro, huh? Acho que seu maior problema não é esse
+	- Açucareiro //Doce como uma chave de metal 
+	- Mala // Os semelhantes se atraem?
+	- Sacola // Contém um bilhete dentro dizendo "Um SACO essa situação, ne?"
+	- Binóculo // "Pra que esses óculos, vovozinha?"
+	- Bilhete // "Às vezes, a vida prega as mesmas peças em lugares diferentes"
+	- Barril // Possui um vinho dentro que está estragado
+	- Boina // Dentro da boina tem uma chave grudada a um bilhete escrito "Bonjour, mon ami"
+	- Cartão de memória // Põe no celular e aparece a senha pra proxima sala com os dizeres "Unagi"
+	- Cômoda // Apenas roupas mofadas, uma barata e um bilhete dizendo "Definitivamente uma situação inCÔMODA"
+	- Diário // 
+	*/
