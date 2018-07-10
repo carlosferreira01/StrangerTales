8362096d8acd774922b8dedb8901aee78d54ef05
Renan Vicente <renanvicentecosta@gmail.com>
2018-07-09 21:43:11 -0300

    New Project

---
+++ sala.h
@@ -0,0 +1,29 @@
+#pragma once
+
+class Sala {
+public:
+	Sala salaEsq;
+	Sala salaDir;
+	Sala salaPai;
+	Sala();
+	~Sala();
+
+
+private:
+	bool aberta;
+};
+
+/* objetos: 
+	- Lampião // Que suas noites sejam tão brilhantes quanto suas futuras ideias
+	- Abajur //Ainda com medo de escuro, huh? Acho que seu maior problema não é esse
+	- Açucareiro //Doce como uma chave de metal 
+	- Mala // Os semelhantes se atraem?
+	- Sacola // Contém um bilhete dentro dizendo "Um SACO essa situação, ne?"
+	- Binóculo // "Pra que esses óculos, vovozinha?"
+	- Bilhete // "Às vezes, a vida prega as mesmas peças em lugares diferentes"
+	- Barril // Possui um vinho dentro que está estragado
+	- Boina // Dentro da boina tem uma chave grudada a um bilhete escrito "Bonjour, mon ami"
+	- Cartão de memória // Põe no celular e aparece a senha pra proxima sala com os dizeres "Unagi"
+	- Cômoda // Apenas roupas mofadas, uma barata e um bilhete dizendo "Definitivamente uma situação inCÔMODA"
+	- Diário // 
+	*/
